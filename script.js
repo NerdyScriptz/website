8a28814ca130f83270e65a4cf34122f83acd8ca7
Gamer 1 <65615643+gamer167@users.noreply.github.com>
2024-05-26 19:30:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,20 +7,20 @@ hamburger.addEventListener('click', () => {
 
 document.onkeydown = function(e) {
 if(event.keyCode == 123) {
+window.alert("No stealing our code!");
 return false;
-window.alert("No stealing our code!")
 }
 if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
+window.alert("No stealing our code!");
 return false;
-window.alert("No stealing our code!")
 }
 if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
+window.alert("No stealing our code!");
 return false;
-window.alert("No stealing our code!")
 }
 if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
+window.alert("No stealing our code!");
 return false;
-window.alert("No stealing our code!")
 }
 }
 //Used to block inspect element with keys
