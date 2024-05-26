58a663ac0155b236773985e2cb4ce6d049a23cfc
BRuve <152766387+BRuveUSMC@users.noreply.github.com>
2024-05-26 11:00:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-script
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+});
